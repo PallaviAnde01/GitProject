262c70c6dd6f3d98b44e4dc2b4076accf00102a3
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-04 19:50:01 +0530

    SMS-03:Extends JapRepository

--- src/main/java/com/PallaviAnde/Repository/StudentDao.java
+++ src/main/java/com/PallaviAnde/Repository/StudentDao.java
@@ -1,5 +1,10 @@
 package com.PallaviAnde.Repository;
 
-public interface StudentDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.PallaviAnde.Model.Student;
+
+@Repository
+public interface StudentDao extends JpaRepository<Student,Integer>{
 
 }
