9ccb43d7f46005532f6d32be1ffc67729476123e
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-04 09:26:46 +0530

    Git Project files

---
+++ src/main/java/com/PallaviAnde/GitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.PallaviAnde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProjectApplication.class, args);
+	}
+
+}
